3b2cac48719ede64196e52bae045817a5beb52f4
2uijin <49094082+2uijin@users.noreply.github.com>
2020-05-06 11:34:31 +0900

    초기 커밋.

---
+++ ilikeswitch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ilikeswitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int level = int.Parse(Console.ReadLine());
+
+            switch (level) { 
+                case 1: Console.WriteLine("수강해야하는 전공 학점 : 12학점");
+                    break;
+                case 2:
+                    Console.WriteLine("수강해야하는 전공 학점 : 18학점");
+                    break;
+                case 3:
+                    Console.WriteLine("수강해야하는 전공 학점 : 10학점");
+                    break;
+                case 4:
+                    Console.WriteLine("수강해야하는 전공 학점 : 18학점");
+                    break;
+            }
+        }
+    }
+}
